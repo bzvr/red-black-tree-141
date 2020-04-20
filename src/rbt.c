1bf102eaa86e19e2ab8fe42734d5926cea68cde3
Maksim <maksimvohmin@mail.ru>
2020-04-20 23:13:56 +0500

    dded functions <<createRBT, tree_search, replace>> in rbt.c

--- src/rbt.c
+++ src/rbt.c
@@ -6,14 +6,18 @@ Node *nilLeaf;
 
 Node *createRBT(void){
     // выделение памяти под черный лист (пустое дерево)
-    // c'mon do something
-    return NULL;
+    Node* tmp = malloc(sizeof(Node));
+    tmp->color = BLACK;
+    tmp->*leftChild = NULL;
+    tmp->*rightChild = NULL;
+    return tmp;
 }
 
 void leftR(Node *x) {
     // левый поворот O(1)
     // c'mon do something
 }
+
 void rightR(Node *x){
     // правый поворот O(1)
     // c'mon do something
@@ -23,14 +27,24 @@ void insert(int value){
     // обычная вставка как в бинарное дерево поиска - новая нода всегда красная с 2 черынми пустыми листьями (O(log n))
     // c'mon do something
 }
+
 void insertFixup(Node *z) {
     // после вставки восстанавливаем свойства кч-дерева (проверяем от вставленной до корня (O(log n))
     // c'mon do something
 }
-void replace(Node *a, Node *b){
+
+void replace(Node* a, Node* b) {
     // свап двух нод
-    // c'mon do something
+    Node* parentA, parentB = b->parent;
+    parentA = a->parent;
+    if (parentA->leftChild == a) parentA->leftChild = b;
+    else parentA->RightChild = b;
+    if (parentB->leftChild == b) parentB->leftChild = a;
+    else parentB->RightChild = a;
+    a->parent = parentB;
+    b->parent = parentA;
 }
+
 void delete(Node *z){
     // удаление нужной вершины (O(log n))
     // c'mon do something
@@ -46,9 +60,21 @@ void clear(Node *x){
     // c'mon do something
 }
 
-Node *search(int value){
+Node* search(Node* root, int value) {
     // поиск ноды по значению - обычный поиск как в бинарном дереве поиска (O(log n))
-    // c'mon do something
+    while (root) {
+        if ((root->value) > (value)) {
+            root = root->left;
+            continue;
+        }
+        else if ((root->value) < value)) {
+        root = root->right;
+        continue;
+        }
+        else {
+            return root;
+        }
+    }
     return NULL;
 }
 Node *min(Node *x){
@@ -56,6 +82,7 @@ Node *min(Node *x){
     // c'mon do something
     return NULL;
 }
+
 Node *max(Node *x){
     // поиск максимума
     // c'mon do something
